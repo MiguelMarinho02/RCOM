1d85e300f134a249d04fca2c5b70e312b42b1fa1
Miguel <up202108822@up.pt>
2023-10-10 09:22:42 +0100

    first commit

---
+++ firstClasses/read_noncanonical.c
@@ -0,0 +1,152 @@
+// Read from serial port in non-canonical mode
+//
+// Modified by: Eduardo Nuno Almeida [enalmeida@fe.up.pt]
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+
+// Baudrate settings are defined in <asm/termbits.h>, which is
+// included by <termios.h>
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 // POSIX compliant source
+
+#define FALSE 0
+#define TRUE 1
+
+#define BUF_SIZE 5
+
+#define FLAG 0x7e
+#define A 0x01
+#define UA 0x07
+
+
+volatile int STOP = FALSE;
+
+int main(int argc, char *argv[])
+{
+    // Program usage: Uses either COM1 or COM2
+    const char *serialPortName = argv[1];
+
+    if (argc < 2)
+    {
+        printf("Incorrect program usage\n"
+               "Usage: %s <SerialPort>\n"
+               "Example: %s /dev/ttyS1\n",
+               argv[0],
+               argv[0]);
+        exit(1);
+    }
+
+    // Open serial port device for reading and writing and not as controlling tty
+    // because we don't want to get killed if linenoise sends CTRL-C.
+    int fd = open(serialPortName, O_RDWR | O_NOCTTY);
+    if (fd < 0)
+    {
+        perror(serialPortName);
+        exit(-1);
+    }
+
+    struct termios oldtio;
+    struct termios newtio;
+
+    // Save current port settings
+    if (tcgetattr(fd, &oldtio) == -1)
+    {
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    // Clear struct for new port settings
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    // Set input mode (non-canonical, no echo,...)
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 0; // Inter-character timer unused
+    newtio.c_cc[VMIN] = 0;  // Blocking read until 5 chars received
+
+    // VTIME e VMIN should be changed in order to protect with a
+    // timeout the reception of the following character(s)
+
+    // Now clean the line and activate the settings for the port
+    // tcflush() discards data written to the object referred to
+    // by fd but not transmitted, or data received but not read,
+    // depending on the value of queue_selector:
+    //   TCIFLUSH - flushes data received but not read.
+    tcflush(fd, TCIOFLUSH);
+
+    // Set new port settings
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("New termios structure set\n");
+
+    // Loop for input
+    unsigned char buf[BUF_SIZE] = {0}; 
+    unsigned char bufs[BUF_SIZE] = {0};
+    
+
+    while (STOP == FALSE)
+    {
+        
+        int bytes = read(fd, buf, BUF_SIZE);
+ 
+        printf("xor= %d\n", buf[3]);
+            
+        printf("1antigo= %d\n", buf[1]);
+            
+        printf("2antigo= %d", buf[2]);
+        
+        if (bytes > 0 && (buf[1] ^ buf[2]) == buf[3]){
+        
+            
+            
+            bufs[0] = FLAG;
+            bufs[1] = A;
+            bufs[2] = UA;
+            bufs[3] = A ^ UA;
+            bufs[4] = FLAG;
+            
+            printf("xor= %d\n", bufs[3]);
+            
+            printf("1= %d\n", bufs[1]);
+            
+            printf("2= %d", bufs[2]);
+            
+            int bytes2 = write(fd, bufs, BUF_SIZE);
+            
+            printf(":%s:%d\n", bufs, bytes2);
+            
+            STOP = TRUE;
+              
+        }
+
+        printf(":%s:%d\n", buf, bytes);
+    }
+
+    // The while() cycle should be changed in order to respect the specifications
+    // of the protocol indicated in the Lab guide
+
+    // Restore the old port settings
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    close(fd);
+
+    return 0;
+}
